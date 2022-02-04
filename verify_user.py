3050f75b06f3f9bfb1a088a385d5482b5ec1a369
Andythompson3 <93014980+Andythompson3@users.noreply.github.com>
2022-02-03 23:06:06 -0700

    feb 3.5

--- verify_user.py
+++ verify_user.py
@@ -5,21 +5,20 @@ banned_group = []
 while unverified_user:
     for user in unverified_user:
         user = user.capitalize()
-        verified_answer = input(f"Is {user} verified? ")
-        verified_answer = verified_answer.capitalize()
+        x = unverified_user.index(user)
+        verified_answer = input(f"Is {user} verified? ").capitalize()
         if verified_answer == "Yes":
-            verified_user = unverified_user.pop()
+            verified_user.append(unverified_user.pop(x))
         else:
-            ban = input(f"Should we ban {user}?")
-            ban = ban.capitalize()
+            ban = input(f"Should we ban {user}?").capitalize()
             if ban == "Yes":
                 print(f"Sorry {user} you are no longer welcome")
-                banned_group = unverified_user.pop()
+                banned_group.append(unverified_user.pop(x))
             else:
                 print("Then they are verified")
-                verified_user = unverified_user.pop()
-        print(f"Unvarified = {unverified_user}")
-        print(f"Varified = {verified_user}")
+                verified_user.append(unverified_user.pop(x))
+        print(f"Unverified = {unverified_user}")
+        print(f"Verified = {verified_user}")
         print(f"Banned = {banned_group}")
 print("Done")
 
